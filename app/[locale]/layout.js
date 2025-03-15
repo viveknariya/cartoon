53a03bd4451e9aed7cbf53675b8dbe6e8237f3ed
Vivek Nariya 🚀 <vhnariya@gmail.com>
2025-03-15 15:11:34 +0530

    google

--- app/[locale]/layout.js
+++ app/[locale]/layout.js
@@ -74,6 +74,21 @@ export default async function RootLayout({ children, params }) {
         <NextIntlClientProvider messages={messages}>
           {children}
         </NextIntlClientProvider>
+        <Script
+          strategy="afterInteractive"
+          src="https://www.googletagmanager.com/gtag/js?id=G-FGBG1FN3WP"
+        />
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+        window.dataLayer = window.dataLayer || [] function gtag()
+        {dataLayer.push(arguments)}
+        gtag('js', new Date()); gtag('config', 'G-FGBG1FN3WP');
+        `,
+          }}
+        />
       </body>
     </html>
   );
